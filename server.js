a6250de90916fe5de471fe32386948a4bf018115
Jinesh Patel <jinesh701@gmail.com>
2018-02-28 00:02:48 -0500

    added body parser and database

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const cors = require('cors');
+
+// Database
+const db = mongoose.connect('mongodb://localhost/bittrack', {
+  useMongoClient: true
+});
 
 const app = express();
 
+// Body Parser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Cors
 app.use(cors());
 
 const PORT = process.env.PORT || 8080;
+
+app.get('/', (req, res) => {
+  res.send('Welcome to my API!');
+});
 
 app.get('/api/*', (req, res) => {
   res.json({ ok: true });
