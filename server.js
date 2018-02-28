eb371a76052acc7f1daa84fe79ceaa80edc3034d
Jinesh Patel <jinesh701@gmail.com>
2018-02-27 23:49:23 -0500

    added cors

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
+app.use(cors());
+
+const PORT = process.env.PORT || 8080;
+
+app.get('/api/*', (req, res) => {
+  res.json({ ok: true });
 });
 
-app.listen(8080, () => {
-  console.log('Listening on port 8080');
-});
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
 module.exports = { app };
