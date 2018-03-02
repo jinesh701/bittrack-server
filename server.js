702052a500995680a07d310ad5f4044d832b095c
Jinesh Patel <jinesh701@gmail.com>
2018-03-02 01:24:01 -0500

    added comments, morgan for logging, and route for watchlist api

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
+// Dependencies
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const cors = require('cors');
+const morgan = require('morgan');
 
-// Database
-const db = mongoose.connect('mongodb://localhost/bittrack', {
-  useMongoClient: true
-});
+const watchlist = require('./router/watchlist');
 
+// MongoDB
+const db = mongoose.connect('mongodb://localhost/bittrack');
+
+// Express
 const app = express();
 
 // Body Parser
@@ -17,14 +21,36 @@ app.use(bodyParser.json());
 // Cors
 app.use(cors());
 
+// Logging
+app.use(morgan('common'));
+
+// Cookie Parser
+app.use(cookieParser());
+
+// Router
+app.use('/api', watchlist);
+
 const PORT = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
   res.send('Welcome to my API!');
 });
 
-app.get('/api/*', (req, res) => {
-  res.json({ ok: true });
+app.post('/login', (req, res) => {
+  let createId = function() {
+    return (
+      '_' +
+      Math.random()
+        .toString(36)
+        .substr(2, 9)
+    );
+  };
+
+  if (!req.cookies.user) {
+    res.cookie('user', createId()).end();
+  } else {
+    res.end();
+  }
 });
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
