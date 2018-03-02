07b1467c808b2c398c3eb57d623a6934ed43ccff
Jinesh Patel <jinesh701@gmail.com>
2018-03-02 02:40:01 -0500

    added config file

---
+++ config.js
@@ -0,0 +1,5 @@
+exports.DATABASE_URL =
+  process.env.DATABASE_URL || 'mongodb://localhost/bittrack';
+exports.TEST_DATABASE_URL =
+  process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-bittrack';
+exports.PORT = process.env.PORT || 8080;
