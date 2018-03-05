06836fefce0bcd8ea953952eea3387ddb0c771c4
Jinesh Patel <jinesh701@gmail.com>
2018-03-05 09:33:07 -0500

    added test for getAllCoins function

--- test/test-api.js
+++ test/test-api.js
@@ -4,12 +4,21 @@ const chai = require('chai');
 
 const expect = chai.expect;
 
-const getCoins = require('../api');
+const { getCoins, getAllCoins } = require('../api');
 
 describe('api function', () => {
   it('should run function and get coin', () => {
     return getCoins('litecoin').then(res => {
       expect(res[0].name).to.equal('Litecoin');
+    });
+  });
+});
+
+describe('#getAllCoins', () => {
+  it('should return all coins information combined with coin symbol', () => {
+    return getAllCoins(['litecoin', 'bitcoin']).then(res => {
+      expect(res[0].name).to.equal('Litecoin');
+      expect(res[1].name).to.equal('Bitcoin');
     });
   });
 });
