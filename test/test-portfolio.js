eda105aaabc4b40e5cc48f8008f504a46cd30c6a
Jinesh Patel <jinesh701@gmail.com>
2018-03-12 22:55:35 -0400

    added test for getFullCryptoPortfolio function

---
+++ test/test-portfolio.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-unused-expressions  */
+
+const chai = require('chai');
+const mongoose = require('mongoose');
+
+const expect = chai.expect;
+
+const { CryptoPortfolio, getFullCryptoPortfolio } = require('../models/portfolio');
+const { TEST_DATABASE_URL } = require('../config');
+
+before(() => mongoose.connect(TEST_DATABASE_URL));
+
+after(() => mongoose.disconnect());
+
+describe('getFullCryptoPortfolio function', () => {
+  it('retrieves updated coin information and includes holdings', () =>
+    CryptoPortfolio.create({ id: 'bitcoin', holdings: 1 })
+      .then(item => getFullCryptoPortfolio().then(portfolios => {
+        expect(portfolios[0].name).to.equal('Bitcoin');
+        expect(portfolios[0].symbol).to.equal('BTC');
+      })));
+});
+
