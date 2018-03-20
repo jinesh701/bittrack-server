22277d419c39e9425f6583f23e9c6d7f03cd5444
Jinesh Patel <jinesh701@gmail.com>
2018-03-20 16:49:40 -0400

    Delete watchlist.js

--- models/watchlist.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const cryptoWatchlistSchema = mongoose.Schema({
-  id: String
-});
-
-const CryptoWatchlist = mongoose.model('Watchlist', cryptoWatchlistSchema);
-
-module.exports = { CryptoWatchlist };
