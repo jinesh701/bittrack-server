53f0eda325bde1c742cf56c83b6a62e2e5c8d848
Jinesh Patel <jinesh701@gmail.com>
2018-03-20 16:44:52 -0400

    added JWT

--- config.js
+++ config.js
@@ -1,6 +1,13 @@
+'use strict';
+
+require('dotenv').config();
+
 exports.DATABASE_URL =
   process.env.DATABASE_URL || 'mongodb://localhost/bittrack';
 exports.TEST_DATABASE_URL =
   process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-bittrack';
 exports.PORT = process.env.PORT || 8080;
 exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3000';
+exports.JWT_SECRET = process.env.JWT_SECRET;
+exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
+
